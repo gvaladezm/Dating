37ed4452e7818801fa855831737e8d8f552d58a6
gvaladezm <gabriel.valadez@gmail.com>
2020-04-22 16:10:48 -0500

    login, toggle register screen

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  ngOnInit() {  }
+
+  registerToggle(){
+    this.registerMode = !this.registerMode;
+  }
+
+  getCancelValue ($event){
+    this.registerMode = $event;
+  }
+
+}
