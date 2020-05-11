928e019b871074f6baa2bd70cf22a7cb2487bb6b
gvaladezm <gabriel.valadez@gmail.com>
2020-05-11 11:37:17 -0500

    progress on reactive forms

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +9,33 @@ import { AlertifyService } from '../_services/alertify.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter<boolean>();
   model: any = {};
-
-   @Output() cancelRegister = new EventEmitter<boolean>();
+  registerForm: FormGroup;
 
   constructor( private authService: AuthService,
                private alertifyService: AlertifyService) {  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required),
+    }, this.passwordMatchValidator);
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true };
+  }
 
   register(){
-    this.authService.register(this.model)
-      .subscribe(() => {
-        this.alertifyService.success('registration successfully');
-      }, error => {
-        this.alertifyService.error(error);
-      });
+    // this.authService.register(this.model)
+    //   .subscribe(() => {
+    //     this.alertifyService.success('registration successfully');
+    //   }, error => {
+    //     this.alertifyService.error(error);
+    //   });
+    console.log(this.registerForm.value);
   }
 
   cancel(){
